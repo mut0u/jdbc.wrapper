e4329b9bd2eae153e72c033dd9380b14be02d8b4
joe <taojoe@gmail.com>
2015-12-16 14:14:41 +0800

    调整api

--- project.clj
+++ project.clj
@@ -5,6 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc1 "0.4.2"]]
-  :main ^:skip-aot jdbc-wrapper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
